22e8ea520cb592c9e2aa00999866658bdb71dfc4
pawut jingjit <pawut.j@gmail.com>
2024-03-09 01:29:00 +0700

    init

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.UserEntity;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends ReactiveCrudRepository<UserEntity, UUID> {
+
+}
---
+++ src/main/resources/user.sql
@@ -0,0 +1,8 @@
+CREATE SCHEMA "app";
+CREATE TABLE "app"."user" (
+    "id" UUID NOT NULL,
+    "username" varchar(50) NOT NULL,
+    "first_name" varchar(50) NOT NULL,
+    "last_name" varchar(50) NOT NULL,
+    PRIMARY KEY ("id")
+);
